538bd0f24e9de400aee387fead7ee451040b900e
Mihai <stmihai98@gmail.com>
2023-09-30 20:11:00 +0300

    feat: set cookie

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
+import buildClient from '../api/build-client';
+
+const FirstPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>First page</h1>;
 };
+
+FirstPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
+
+export default FirstPage;
