43974c8488e763a9eae608f256e2e4d91dd9da54
Mihai <stmihai98@gmail.com>
2023-10-25 18:56:48 +0300

    refactor: typescript checks

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,14 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { Subjects } from './subjects';
+import { TicketCreatedEvent } from './ticket-created-event';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  // readonly in ts is like final in java
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
-  onMessage(data: any, msg: Message) {
-    console.log('Event data: ' + data);
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    console.log('Event data: ' + JSON.stringify(data));
 
     msg.ack();
   }
